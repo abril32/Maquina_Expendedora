41c1d1c334e5df9a024faf22368aeb7b7cfc3c0c
Abril Mosquera <lagartosautos@gmail.com>
2025-11-13 10:25:16 -0300

    Avance con el pago de paypal

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,6 +1,10 @@
+import { Value } from "@radix-ui/react-select";
+import { Description } from "@radix-ui/react-toast";
 import axios from "axios"
+import { Currency } from "lucide-react";
 import qs from "qs"
 import { preconnect } from "react-dom";
+import { FunnelChart } from "recharts";
 
 const headers = {
   'Content-Type': 'application/x-www-form-urlencoded',
@@ -18,5 +22,39 @@ const respuesta = await axios.post(url, data, {
     password: process.env.PAYPAL_CLIENT_SECRET || "pass",
   },
 })
-  console.log(respuesta.data);
+  return respuesta.data.access_token
+}
+
+
+export async function crearOrden() {
+  const access_token = generaTokenAcceso()
+
+  const url = 'https://api-m.sandbox.paypal.com/v2/checkout/orders'
+
+  const headers = {
+    'Content-Type': 'application/json', 
+    'Authorization': 'Bearer' + access_token,
+  }
+/*  editar / cambiar a tsx
+  const data = qs.stringify{
+    intent: 'CAPTURE',
+    purchase_units= [
+      {
+        items: [
+          {
+            name: 'Pagar Ahora',
+            Description: '',
+            quantity: 1,
+            unit_amount: {
+              Currency_code: 'USD',
+              Value: '400'
+            }
+          }
+        ]
+      }
+    ]
+  }*/
+  const respuesta = await axios.post(url, headers
+    
+  )
 }
